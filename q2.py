55283df86741845c1613440c82082d1eb0e9cb60
walquer244 <isaac.sousa77@aluno.ce.gov.br>
2025-09-02 11:56:23 -0300

    commit

--- q2.py
+++ q2.py
@@ -1,7 +1,7 @@
 #Questão – Controle de Vendas
 contador = 0
 quantidade_de_produtos = input("Digite a quantidade de produtos que foram vendidos: ")
-for i in quantidade_de_produtos:
+for i i quantidade_de_produtos:
     valor_dos_produtos = float(input("Digite aqui, o valor de cada produto vendido: "))
     contador += 1 
 
